3d53d723ee005106bb20b6d125f157ec5f3f79a5
Ginthozan Varnakulasingam <ginthozanv@gmail.com>
2022-08-07 20:20:10 +0530

    ads

--- src/index.js
+++ src/index.js
@@ -1,35 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
-var bodyParser = require('body-parser')
+const express = require("express");
+const mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 // const url = 'mongodb://localhost:27017/ramany-boutique'
-const url = "mongodb+srv://yathushan:BQdEfkQANH6kT3Nu@cluster0.hodq8.mongodb.net/ramany-boutique?retryWrites=true&w=majority";
-const commonrouter = require('./router')
-var cors = require('cors')
-
+const url =
+	"mongodb+srv://yathushan:BQdEfkQANH6kT3Nu@cluster0.hodq8.mongodb.net/ramany-boutique?retryWrites=true&w=majority";
+const commonrouter = require("./router");
+var cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001;
 
-app.use(cors())
+app.use(cors());
 
-databaseConnection()
-serve()
+databaseConnection();
+serve();
 
 function databaseConnection() {
-    mongoose.connect(url, { useNewUrlParser: true })
-    const con = mongoose.connection
+	mongoose.connect(url, { useNewUrlParser: true });
+	const con = mongoose.connection;
 
-    con.on('open', function () {
-       console.log('Database Connected>>>>>>>>>>>>')
-    })
+	con.on("open", function () {
+		console.log("Database Connected>>>>>>>>>>>>");
+	});
 }
 
-app.use(bodyParser.json())    
-app.use('/', commonrouter)
+app.use(bodyParser.json());
+app.use("/", commonrouter);
 
-
-function serve(){
-app.listen(port,()=>{
-    console.log("Server started")
-})
+function serve() {
+	app.listen(port, () => {
+		console.log("Server started");
+	});
 }
